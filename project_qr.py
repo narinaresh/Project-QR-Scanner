6cef3154e8ca5e3be3e58b9485769cf09094c822
narinaresh325 <narinaresh0302@gmail.com>
2024-06-29 19:56:31 +0530

    jsut played with the list of the data

--- project_qr.py
+++ project_qr.py
@@ -2,9 +2,10 @@
 
 from pyzbar.pyzbar import decode #Importing decode function to decode the QR code
 from PIL import Image #Importing Image function to open the image 
+import cv2
 
 # opening a image 
-img_path = "naresh.png"
+img_path = "detaills.png"
 image = Image.open(img_path)
 
 #decoding the image using decode function
@@ -12,4 +13,6 @@ img_decode = decode(image)
 
 # reading the data inside the QR which in the form of utf-8 encoding and decoding format
 for de_img in img_decode:
-    print(de_img.data.decode("utf-8"))
+    print(de_img.data.decode("utf-8")[0:])
+
+print(cv2.__version__)
