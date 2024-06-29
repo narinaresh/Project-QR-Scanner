c2765c69530d1fb1ed7f5769ea232965e122d564
narinaresh325 <narinaresh0302@gmail.com>
2024-06-29 21:07:02 +0530

    properly capturing and give the information of the qr code

--- project_qr.py
+++ project_qr.py
@@ -1,18 +1,44 @@
 # Project QR (quick response) scanner
+#importing required libraries
 
-from pyzbar.pyzbar import decode #Importing decode function to decode the QR code
-from PIL import Image #Importing Image function to open the image 
-import cv2
+import os 
+from pyzbar.pyzbar import decode 
+from PIL import Image 
+import cv2 
+import winsound 
+
+image_path  = "cap1.png" #image path
+
+cap = cv2.VideoCapture(0) 
+
+#check camera opening
+if not cap.isOpened():
+    print("\033[91mERROR:\033[0m Failed to open Camera!!")
+    exit()
+
+#capture the image
+ret, frame = cap.read()
+
+#check it's captured
+if not ret:
+    print("\033[91mERROR:\033[0m Failed to capture frame.")
+    exit()
+
+cap.release()
+
+#saving the image
+cv2.imwrite(image_path, frame)
+
+if os.path.exists(image_path):
+    print(f"Image saved successfully as {image_path}")
+    winsound.Beep(1000,200)
 
 # opening a image 
-img_path = "detaills.png"
-image = Image.open(img_path)
+image = Image.open(image_path)
 
 #decoding the image using decode function
 img_decode = decode(image)
 
-# reading the data inside the QR which in the form of utf-8 encoding and decoding format
+# reading the data inside the QR which is in the form unicode
 for de_img in img_decode:
     print(de_img.data.decode("utf-8")[0:])
-
-print(cv2.__version__)
