3c7a21f097cdf14fb19c5c07ef0cfeb070067bc6
narinaresh325 <narinaresh0302@gmail.com>
2024-06-29 19:45:54 +0530

    first commit

---
+++ project_qr.py
@@ -0,0 +1,15 @@
+# Project QR (quick response) scanner
+
+from pyzbar.pyzbar import decode #Importing decode function to decode the QR code
+from PIL import Image #Importing Image function to open the image 
+
+# opening a image 
+img_path = "naresh.png"
+image = Image.open(img_path)
+
+#decoding the image using decode function
+img_decode = decode(image)
+
+# reading the data inside the QR which in the form of utf-8 encoding and decoding format
+for de_img in img_decode:
+    print(de_img.data.decode("utf-8"))
