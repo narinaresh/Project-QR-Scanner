b4bda9f41e44f2bd52cdb77244c63b50504fe24e
narinaresh325 <narinaresh0302@gmail.com>
2024-06-30 16:54:27 +0530

    continuous capturing and reading of QR code for each 5 to 6 second lag because of RTOS and need to add long beep if the qr code didnt read or anyother soluction

--- project_qr.py
+++ project_qr.py
@@ -1,44 +1,52 @@
 # Project QR (quick response) scanner
-#importing required libraries
+# importing required libraries
 
-import os 
-from pyzbar.pyzbar import decode 
-from PIL import Image 
-import cv2 
-import winsound 
+import keyboard
+import time
+import os
+from pyzbar.pyzbar import decode
+from PIL import Image
+import cv2
+import winsound
 
-image_path  = "cap1.png" #image path
+count = 0
 
-cap = cv2.VideoCapture(0) 
+while(1):
 
-#check camera opening
-if not cap.isOpened():
-    print("\033[91mERROR:\033[0m Failed to open Camera!!")
-    exit()
+    cap = cv2.VideoCapture(0)
+    
+    # check camera opening
+    if not cap.isOpened():
+        print("\033[91mERROR:\033[0m Failed to open Camera!!")
+        exit()
 
-#capture the image
-ret, frame = cap.read()
+    image_path = f"cap{count}.png"  # image path
 
-#check it's captured
-if not ret:
-    print("\033[91mERROR:\033[0m Failed to capture frame.")
-    exit()
+    # capture the image
+    ret, frame = cap.read()
+
+    # check it's captured
+    if not ret:
+        print("\033[91mERROR:\033[0m Failed to capture frame.")
+        exit()
+        
+    # saving the image
+    cv2.imwrite(image_path, frame)
+
+    if os.path.exists(image_path):
+        print(f"Image saved successfully as {image_path}")
+        winsound.Beep(1000, 200)
+
+    # opening a image
+    image = Image.open(image_path)
+
+    # decoding the image using decode function
+    img_decode = decode(image)
+
+    # reading the data inside the QR which is in the form unicode
+    for de_img in img_decode:
+        print(de_img.data.decode("utf-8")[0:])
+
+    count = count + 1
 
 cap.release()
-
-#saving the image
-cv2.imwrite(image_path, frame)
-
-if os.path.exists(image_path):
-    print(f"Image saved successfully as {image_path}")
-    winsound.Beep(1000,200)
-
-# opening a image 
-image = Image.open(image_path)
-
-#decoding the image using decode function
-img_decode = decode(image)
-
-# reading the data inside the QR which is in the form unicode
-for de_img in img_decode:
-    print(de_img.data.decode("utf-8")[0:])
